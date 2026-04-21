d1d95b7c8abbb47aac6abd4b4319932665f1aa9b
H. Watanabe <kaityo@users.sourceforge.jp>
2026-04-21 14:52:12 +0900

    updates

--- hostname/test.sh
+++ hostname/test.sh
@@ -1,6 +1,3 @@
 #!/bin/bash
-#PBS -l nodes=1:ppn=20
-
-cd $PBS_O_WORKDIR
 
 mpirun -np 20 ./a.out
--- pi/pi.sh
+++ pi/pi.sh
@@ -1,6 +1,3 @@
 #!/bin/bash
-#PBS -l nodes=1:ppn=20
-
-cd $PBS_O_WORKDIR
 hostname
 mpirun -np 20 ./cps/cps task.sh
